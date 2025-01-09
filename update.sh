33d9a351e5ab62cea2138635998b6deee2697bf2
Chris McFee <ggmethos@autistici.org>
2025-01-09 13:51:28 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,35 @@
-#/bin/bash
+#!/bin/bash
+
+# Update package lists
 sudo chmod 666 /dev/video*
+
+# Install required packages
+sudo apt-get install -y sshpass ansible git
+
+# Create a directory for the GitHub repo (optional)
+mkdir -p ~/git_repo
+
+# Clone the public GitHub repo (replace with your repo URL)
+git clone https://github.com/chrismcfee/camerapub.git
+
+# Create target directory if it doesn't exist
+#mkdir -p ~/target_directory
+
+# Copy 3 files from repo to target directory
+cp ~/camerapub/camera-setup.sh camera_binder/
+cp ~/camerapub/camera_binder.service camera_binder/
+cp ~/camerapub/camera_binder.py camera_binder/
+
+# Change directory to repo directory
+cd ~/camerapub
+
+# Run ansible playbook
+ansible-playbook nomesh.yaml -i invetory-players-version-2.ini
+
+# Go to home directory
+cd ~
+
+# Run script (replace with your script name)
+./wf_vidireports-7.7.8.4-bundle_x86_64_network_1440.sh
+
+cat /home/zignage/.vidireports/VidiReports.log | grep box_id
