7ce2c41e7e3929a8b95c5a5c28f910238d76fedb
Chris McFee <ggmethos@autistici.org>
2025-01-23 06:41:07 -0500

    Update update_camera_binding_only.sh

--- update_camera_binding_only.sh
+++ update_camera_binding_only.sh
@@ -217,17 +217,19 @@ echo "starting"
 VIDIREPORTS_PATH="/home/zignage/.vidireports/7.7.8.4/./vidireports"
 LOG_PATH="/home/zignage/.vidireports/VidiReports.log"
 CONFIG_PATH="/home/zignage/.vidireports/config/"
-sudo tee /etc/systemd/system/vidireports.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/vidireports.service > /dev/null <<'EOF'
 [Unit]
 Description=VidiReports Service
 After=network.target
+
 [Service]
 Type=simple
-ExecStart=$VIDIREPORTS_PATH -d -l $LOG_PATH -c $CONFIG_PATH
+ExecStart=/home/zignage/.vidireports/7.7.8.4/./vidireports -d -l /home/zignage/.vidireports/VidiReports.log -c /home/zignage/.vidireports/config/
 Restart=on-failure
 User=zignage
 TimeoutStopSec=30
 Group=video
+
 [Install]
 WantedBy=multi-user.target
 EOF
