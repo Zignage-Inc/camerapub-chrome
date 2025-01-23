2cadd5a5e21cba25eb5e6eca46427571e4d911e5
Chris McFee <ggmethos@autistici.org>
2025-01-23 07:04:12 -0500

    Update update_camera_binding_only.sh

--- update_camera_binding_only.sh
+++ update_camera_binding_only.sh
@@ -217,22 +217,6 @@ echo "starting"
 VIDIREPORTS_PATH="/home/zignage/.vidireports/7.7.8.4/./vidireports"
 LOG_PATH="/home/zignage/.vidireports/VidiReports.log"
 CONFIG_PATH="/home/zignage/.vidireports/config/"
-sudo bash -c 'cat > /etc/systemd/system/vidireports.service <<EOL
-[Unit]
-Description=VidiReports Service
-After=network.target
-
-[Service]
-Type=simple
-ExecStart=/home/zignage/.vidireports/7.7.8.4/./vidireports -d -l /home/zignage/.vidireports/VidiReports.log -c /home/zignage/.vidireports/config/
-Restart=on-failure
-User=zignage
-TimeoutStopSec=30
-Group=video
-
-[Install]
-WantedBy=multi-user.target
-EOL'
 
 # Reload systemd daemon and restart service
 sudo systemctl daemon-reload
